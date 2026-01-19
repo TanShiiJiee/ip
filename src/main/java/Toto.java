c3eb7878f3c0dced77aa2bf3314e8331702cec55
TanShiiJiee <e1356537@u.nus.edu>
2026-01-19 14:44:13 +0800

    Changed chatbot name and implement greeting before exiting

---
+++ src/main/java/Toto.java
@@ -0,0 +1,10 @@
+public class Toto {
+    public static void main(String[] args) {
+        String start = "___________________________________________________________ \n"
+                + "Hello! I'm Toto \n"
+                + "What can I do for you? \n"
+                + "___________________________________________________________ \n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(start);
+    }
+}
